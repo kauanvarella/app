abb4f7cda6245e30ed5a8c1e720a376b8e09c8a9
kauanvarella <kauanvarella@gmail.com>
2022-07-24 22:30:34 -0300

    3P6D

--- www/Scripts/login.php
+++ www/Scripts/login.php
@@ -1,12 +1,12 @@
 <?php
 
-    $db =  new PDO('mysql:host=34.211.224.42:3306;dbname=dbprojeto;charset=utf8', 'root', 'root');
+    $db =  new PDO('mysql:host=localhost:3306;dbname=dbprojeto;charset=utf8', 'root', 'root');
     $consulta = $db->query('SELECT usuario, senha FROM cadastros');
     $listaconsulta = $consulta->fetchAll();
 
     foreach($listaconsulta as $item){
         if($item['usuario'] == $_POST['user'] && $item['senha'] == $_POST['senha']){
-            header("location:34.211.224.42/index.php");
+            header("location:index.php?valor=1");
             break;
         };
         header("location:index.php?valor=1");
