4785f3e236f417d7046dbe9c22279efd669dd84a
kauanvarella <kauanvarella@gmail.com>
2022-07-25 01:27:15 -0300

    2P6D

--- www/Scripts/cadastro.php
+++ www/Scripts/cadastro.php
@@ -1,42 +1,45 @@
 <?php
 
-    $db =  new PDO('mysql:localhost;dbname=dbprojeto;charset=utf8', 'root', 'root');
-    $consulta = $db->query('SELECT usuario, senha FROM cadastros');
-    $listaconsulta = $consulta->fetchAll();
-    $cadastra = true;
+    try {
+        $db =  new PDO('mysql:localhost;dbname=dbprojeto;charset=utf8', 'root', 'root');
+        $consulta = $db->query('SELECT usuario, senha FROM cadastros');
+        $listaconsulta = $consulta->fetchAll();
+        $cadastra = true;
+        
+        foreach($listaconsulta as $item){
+            if($item['usuario'] == $_POST['defineusuario']){
+                $cadastra = false;
+                header("location:../index.php?modal=1");
+                $valor = 1; 
+                break;
+            }
+        };
     
-    foreach($listaconsulta as $item){
-        if($item['usuario'] == $_POST['defineusuario']){
+    
+        if($_POST['definesenha'] != $_POST['confirmasenha']){
             $cadastra = false;
-            header("location:../index.php?modal=1");
-            $valor = 1; 
-            break;
-        }
-    };
-
-
-    if($_POST['definesenha'] != $_POST['confirmasenha']){
-        $cadastra = false;
-        header("location:../index.php?modal=2");
-    } 
-
-
-    if($_POST['definesenha'] != $_POST['confirmasenha'] && $valor == 1){
-        $cadastra = false;
-        header("location:../index.php?modal=12");
-    } 
-
-
-    if ($_POST['defineusuario'] == null || $_POST['definesenha'] == null){
-        $cadastra = false;
-        header("location:../index.php?modal=20");
-   } 
-   
-
-   if ($cadastra == true){
-        $db->query("INSERT INTO cadastros (usuario, senha) VALUES ('{$_POST['defineusuario']}','{$_POST['definesenha']}');");
-        header("location:../index.php?sucesso=1");
-   }
-
+            header("location:../index.php?modal=2");
+        } 
+    
+    
+        if($_POST['definesenha'] != $_POST['confirmasenha'] && $valor == 1){
+            $cadastra = false;
+            header("location:../index.php?modal=12");
+        } 
+    
+    
+        if ($_POST['defineusuario'] == null || $_POST['definesenha'] == null){
+            $cadastra = false;
+            header("location:../index.php?modal=20");
+       } 
+       
+    
+       if ($cadastra == true){
+            $db->query("INSERT INTO cadastros (usuario, senha) VALUES ('{$_POST['defineusuario']}','{$_POST['definesenha']}');");
+            header("location:../index.php?sucesso=1");
+       }
+    } catch (PDOException $e) {
+        echo 'ERRO: ' . $e->getMessage();
+    }
 
 ?>
