4b01b1cb2563dc39972352f08bd25258911178df
kauanvarella <kauanvarella@gmail.com>
2022-07-24 22:45:42 -0300

    3P6D

--- www/Scripts/login.php
+++ www/Scripts/login.php
@@ -1,18 +1,21 @@
 <?php
 
-    $db = new PDO('mysql:host=localhost;dbname=dbprojeto', 'root', 'root');
-    $consulta = $db->query('SELECT usuario, senha FROM cadastros');
-    $listaconsulta = $consulta->fetchAll();
-
-    foreach($listaconsulta as $item){
-        if($item['usuario'] == $_POST['user'] && $item['senha'] == $_POST['senha']){
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=dbprojeto', 'root', 'root');
+        $consulta = $db->query('SELECT usuario, senha FROM cadastros');
+        $listaconsulta = $consulta->fetchAll();
+    
+        foreach($listaconsulta as $item){
+            if($item['usuario'] == $_POST['user'] && $item['senha'] == $_POST['senha']){
+                header("location:index.php?valor=1");
+                break;
+            };
             header("location:index.php?valor=1");
-            break;
         };
-        header("location:index.php?valor=1");
-    };
+    } catch (PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
 
-   
 ?>
 
 
