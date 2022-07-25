324f07a28a4e1ba0154b857037fdc0d82a37f652
kauanvarella <kauanvarella@gmail.com>
2022-07-25 02:24:01 -0300

    3P6D

--- www/Scripts/login.php
+++ www/Scripts/login.php
@@ -1,7 +1,7 @@
 <?php
 
     try {
-        $db = new PDO('mysql:host=172.17.0.4;dbname=dbprojeto', 'root', 'root');
+        $db = new PDO('mysql:host=172.17.0.4;dbname=dbprojeto', 'root', 'password');
         $consulta = $db->query('SELECT usuario, senha FROM cadastros');
         $listaconsulta = $consulta->fetchAll();
     
