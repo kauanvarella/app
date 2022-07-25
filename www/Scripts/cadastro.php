d6e143127240a271bb69aa36816384d18ef27eb9
kauanvarella <kauanvarella@gmail.com>
2022-07-25 01:21:59 -0300

    2P6D

--- www/Scripts/cadastro.php
+++ www/Scripts/cadastro.php
@@ -8,7 +8,7 @@
     foreach($listaconsulta as $item){
         if($item['usuario'] == $_POST['defineusuario']){
             $cadastra = false;
-            header("location:index.php?modal=1");
+            header("location:../index.php?modal=1");
             $valor = 1; 
             break;
         }
@@ -17,25 +17,25 @@
 
     if($_POST['definesenha'] != $_POST['confirmasenha']){
         $cadastra = false;
-        header("location:index.php?modal=2");
+        header("location:../index.php?modal=2");
     } 
 
 
     if($_POST['definesenha'] != $_POST['confirmasenha'] && $valor == 1){
         $cadastra = false;
-        header("location:index.php?modal=12");
+        header("location:../index.php?modal=12");
     } 
 
 
     if ($_POST['defineusuario'] == null || $_POST['definesenha'] == null){
         $cadastra = false;
-        header("location:index.php?modal=20");
+        header("location:../index.php?modal=20");
    } 
    
 
    if ($cadastra == true){
         $db->query("INSERT INTO cadastros (usuario, senha) VALUES ('{$_POST['defineusuario']}','{$_POST['definesenha']}');");
-        header("location:index.php?sucesso=1");
+        header("location:../index.php?sucesso=1");
    }
 
 
