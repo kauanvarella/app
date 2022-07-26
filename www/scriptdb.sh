f104accafcd9349362272d2a748873606cee5c0f
kauanvarella <kauanvarella@gmail.com>
2022-07-26 05:35:26 -0300

    3P7D

--- www/scriptdb.sh
+++ www/scriptdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo docker exec -it db bash \
-mysql -u root -proot \
-create database dbprojeto; \
-use dbprojeto; \
+sudo docker exec -it db bash &&
+mysql -u root -proot &&
+create database dbprojeto; &&
+use dbprojeto; &&
 create table cadastros(cod INT NOT NULL AUTO_INCREMENT PRIMARY KEY, usuario VARCHAR(50) NOT NULL, senha VARCHAR(50) NOT NULL);
