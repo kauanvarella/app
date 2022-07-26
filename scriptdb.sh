7b9ae9fc45d0219c7e05d55b912ac42ad6f884bf
kauanvarella <kauanvarella@gmail.com>
2022-07-26 06:06:35 -0300

    3P7D

--- scriptdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo docker exec -it db bash
-mysql -u root -proot
-create database dbprojeto;
-use dbprojeto;
-create table cadastros(cod INT NOT NULL AUTO_INCREMENT PRIMARY KEY, usuario VARCHAR(50) NOT NULL, senha VARCHAR(50) NOT NULL);
